c1c44ce7b4b52275580857e3b4aa4944d37e5b26
EvandroRBR <evandro.braga01@fatec.sp.gov.br>
2022-06-13 14:15:54 -0300

    inicio de um projeto

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+
+import swaggerFile from './swagger.json';
+
+const app = express();
+
+app.use(express.json());
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
+app.get('/', (request, response) => response.json({ message: 'ok' }));
+
+app.listen('3333', () =>
+  console.info('\x1b[32m', '\x1b[1m', 'Server is running! 🚀', '\x1b[0m'),
+);
